3da019a21e34da2a3f6592eefc9d12432e66f1c6
Amir Rostami <amr.rostam@gmail.com>
2024-03-09 23:50:35 +0330

    Added Keywords

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,12 +6,28 @@ import { PageLang } from '@/interface/General.model'
 import { appViewport } from '@/constants'
 import HomeContainer from '@/modules/home/containers/Home'
 
+const keywords = [
+  'تولید محتوا هوش مصنوعی',
+  'ابزار تولید محتوا هوش مصنوعی',
+  'سایت تولید محتوا هوش مصنوعی',
+  'برنامه تولید محتوا هوش مصنوعی',
+  'تولید محتوا با هوش مصنوعی',
+  'هوش مصنوعی تولید محتوا رایگان',
+  'هوش مصنوعی برای تولید محتوا',
+  'هوش مصنوعی تولید محتوا فارسی',
+  'تولید محتوا هوش مصنوعی رایگان',
+  'معرفی هوش مصنوعی برای تولید محتوا',
+  'تولید محتوا اینستاگرام',
+  'تولید محتوای اینستاگرام',
+  'تولید محتوا در اینستاگرام',
+]
+
 export async function generateMetadata({ params: { locale } }: PageLang): Promise<Metadata> {
   const t = await getTranslations({ locale, namespace: 'Metadata' })
 
   return {
     title: t('Home.Title'),
-    keywords: t('Home.Keywords'),
+    keywords: `${t('Home.Keywords')} ${keywords.join(', ')}`,
     description: t('Home.Description'),
   }
 }

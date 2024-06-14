fd397cf34bb36a2523c3cfe01d9f57b2f9be6f26
Amir Rostami <amr.rostam@gmail.com>
2024-06-14 11:10:07 +0330

    Fixed Coming Soon

--- src/providers/ThemProvider/index.tsx
+++ src/providers/ThemProvider/index.tsx
@@ -6,19 +6,14 @@ import { useParams } from 'next/navigation'
 
 import * as React from 'react'
 
-import ComingSoonPage from '@/modules/common/containers/ComingSoon'
-
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const { locale } = useParams()
-  const [isComingSoon, setIsComingSoon] = React.useState(false)
 
   React.useEffect(() => {
-    setIsComingSoon(window.location.origin.includes('https://ainevis.com'))
-
     if (locale) {
       localStorage.setItem('lang', locale as string)
     }
   }, [locale])
 
-  return <NextThemesProvider {...props}>{isComingSoon ? <ComingSoonPage /> : children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
